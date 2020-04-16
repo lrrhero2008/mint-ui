d4b87fd82ad56a7721cc35dbf9864cee95e6bc13
Alex <lrrhero@qq.com>
2020-04-16 10:51:59 +0800

    [build] 2.2.20

--- src/index.js
+++ src/index.js
@@ -34,7 +34,7 @@ import PaletteButton from '../packages/palette-button';
 import '../src/assets/font/iconfont.css';
 import merge from './utils/merge';
 
-const version = '2.2.19';
+const version = '2.2.20';
 const install = function(Vue, config = {}) {
   if (install.installed) return;
 
